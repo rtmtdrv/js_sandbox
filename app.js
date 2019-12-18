d48bb1e11089ecae2d2daad02c5de8193bbd2ce9
Art Todorov <art.todorov@me.com>
2019-12-17 22:16:35 -0800

    variables

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/*
+
 // alert('Hello World'); 
 
 // Log to console
@@ -28,6 +30,60 @@ console.time('Hello');
   console.log('Hello World');
   console.log('Hello World');
 console.timeEnd('Hello');
+
+*/
+
+// // var, let, const
+// var name = 'John Doe'
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// // Init Variable
+// var greeting;
+// console.log(greeting);
+// greeting =  'hello';
+// console.log(greeting);
+
+// // Letters, numbers, _, $
+// // Can not start with number
+
+// // Multi word vars
+// var firstName = 'John'; // Camel case
+// var first_name = 'Sara'; // Underscore
+// var FirstName = 'Tom'; // Pascal case
+
+// Let
+
+// let name;
+// name = 'John Doe'
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// Const
+const name = 'John';
+console.log(name);
+// Can not reassign
+// name = 'Sara';
+// Have to assign value
+// const greeting;
+
+const person = {
+  name: 'John',
+  age: 30
+}
+
+person.name = 'Sara';
+person.age = 32;
+
+// console.log(person);
+
+const numbers = [1,2,3,4,5];
+numbers.push(6);
+
+console.log(numbers);
+
 
 
 
