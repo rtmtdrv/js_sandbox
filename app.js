56c32b0951556b6285964ef83d02c1e27306bfe4
Art Todorov <art.todorov@me.com>
2019-12-17 19:09:14 -0800

    using the console

---
+++ app.js
@@ -0,0 +1,35 @@
+// alert('Hello World'); 
+
+// Log to console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello'
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+
+// Table
+console.table({a:1, b:2});
+
+// Error
+console.error('This is some error');
+
+// Clear
+console.clear();
+console.warn('This is a warning');
+
+// Time
+console.time('Hello');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+console.timeEnd('Hello');
+
+
+
+
+
