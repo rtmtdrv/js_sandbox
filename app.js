0554641144084db74cb5938856e432c22bd150f3
Art Todorov <art.todorov@me.com>
2019-12-18 20:44:55 -0800

    type conversion

--- app.js
+++ app.js
@@ -100,56 +100,87 @@ console.timeEnd('Hello');
 
 // Primitive
 
-// String
-const name = 'Jon Doe'
-console.log(typeof name);
+// // String
+// const name = 'Jon Doe'
+// console.log(typeof name);
 
-// Number
-const age = 45;
-console.log(typeof age);
+// // Number
+// const age = 45;
+// console.log(typeof age);
 
-// Boolean
-const hasKids = true;
-console.log(typeof hasKids);
+// // Boolean
+// const hasKids = true;
+// console.log(typeof hasKids);
 
-// Null
-const car = null;
-console.log(typeof car);
+// // Null
+// const car = null;
+// console.log(typeof car);
 
-// Undefined
-let test;
-console.log(typeof test);
+// // Undefined
+// let test;
+// console.log(typeof test);
 
-// Symbol
-const sym = Symbol();
-console.log(typeof sym);
+// // Symbol
+// const sym = Symbol();
+// console.log(typeof sym);
 
-// REFERENCE TYPES - Objects
-// Array
-const hobbies = ['movies', 'music'];
-console.log(typeof hobbies);
+// // REFERENCE TYPES - Objects
+// // Array
+// const hobbies = ['movies', 'music'];
+// console.log(typeof hobbies);
 
-// Object Literal
-const address = {
-  city: 'Yorba Linda',
-  state: 'CA'
-}
-console.log(typeof address);
+// // Object Literal
+// const address = {
+//   city: 'Yorba Linda',
+//   state: 'CA'
+// }
+// console.log(typeof address);
 
-// Date
-const today = new Date();
-console.log(today);
-console.log(typeof today);
+// // Date
+// const today = new Date();
+// console.log(today);
+// console.log(typeof today);
 
+let val;
 
+// Number to string
+val = String(555);
+val = String(4+4);
+// Bool to string
+val = String(true);
+// Date to string
+val = String(new Date());
+// Array to string
+val = String([1,2,3,4])
 
+// toString()
+val = (5).toString();
+val = (true).toString();
 
+// String to number
+val = Number('5');
+val = Number(true);
+val = Number(true);
+val = Number(null);
+val = Number('hello');
+val = Number([1,2,3]);
 
+val = parseInt('100');
+val = parseFloat('100.30');
 
+// Output 
+// console.log(val);
+// console.log(typeof val);
+// // console.log(val.length);
+// console.log(val.toFixed(2));
 
+// Type Coersion
+const val1 = String(5);
+const val2 = 6;
+const sum = Number(val1 + val2);
 
-
-
+console.log(sum);
+console.log(typeof sum);
 
 
 
