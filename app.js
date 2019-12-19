ee8e7ee31c47a19754ed42eee165d0a00bb5e4ae
Art Todorov <art.todorov@me.com>
2019-12-18 20:53:24 -0800

    numbers & math object

--- app.js
+++ app.js
@@ -141,47 +141,74 @@ console.timeEnd('Hello');
 // console.log(today);
 // console.log(typeof today);
 
+// let val;
+
+// // Number to string
+// val = String(555);
+// val = String(4+4);
+// // Bool to string
+// val = String(true);
+// // Date to string
+// val = String(new Date());
+// // Array to string
+// val = String([1,2,3,4])
+
+// // toString()
+// val = (5).toString();
+// val = (true).toString();
+
+// // String to number
+// val = Number('5');
+// val = Number(true);
+// val = Number(true);
+// val = Number(null);
+// val = Number('hello');
+// val = Number([1,2,3]);
+
+// val = parseInt('100');
+// val = parseFloat('100.30');
+
+// // Output 
+// // console.log(val);
+// // console.log(typeof val);
+// // // console.log(val.length);
+// // console.log(val.toFixed(2));
+
+// // Type Coersion
+// const val1 = String(5);
+// const val2 = 6;
+// const sum = Number(val1 + val2);
+
+// console.log(sum);
+// console.log(typeof sum);
+
+const num1 = 100;
+const num2 = 50;
 let val;
 
-// Number to string
-val = String(555);
-val = String(4+4);
-// Bool to string
-val = String(true);
-// Date to string
-val = String(new Date());
-// Array to string
-val = String([1,2,3,4])
+// Simple Math with numbers
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
 
-// toString()
-val = (5).toString();
-val = (true).toString();
+// Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(8, 2);
+val = Math.min(2, 33, 4, 1, 55, 6, 3, -2);
+val = Math.max(2, 33, 4, 1, 55, 6, 3, -2);
+val = Math.random();
 
-// String to number
-val = Number('5');
-val = Number(true);
-val = Number(true);
-val = Number(null);
-val = Number('hello');
-val = Number([1,2,3]);
+val = Math.floor(Math.random() * 20 + 1);
 
-val = parseInt('100');
-val = parseFloat('100.30');
-
-// Output 
-// console.log(val);
-// console.log(typeof val);
-// // console.log(val.length);
-// console.log(val.toFixed(2));
-
-// Type Coersion
-const val1 = String(5);
-const val2 = 6;
-const sum = Number(val1 + val2);
-
-console.log(sum);
-console.log(typeof sum);
-
+console.log(val);
 
 
 
