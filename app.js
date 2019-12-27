235846782898ce5a355a641a7d4c51a78a10588e
Art Todorov <art.todorov@me.com>
2019-12-26 22:04:13 -0800

    if statements and comparison operators

--- app.js
+++ app.js
@@ -417,28 +417,122 @@ console.timeEnd('Hello');
 
 // DATES AND TIMES
 
-let val;
+// let val;
 
-const today = new Date();
-let birthday = new Date('9-10-1981 11:25:00');
-birthday = new Date('September 10 1981');
-birthday = new Date('9/10/1981');
+// const today = new Date();
+// let birthday = new Date('9-10-1981 11:25:00');
+// birthday = new Date('September 10 1981');
+// birthday = new Date('9/10/1981');
 
-val = today.getMonth();
-val = today.getDate();
-val = today.getDay();
-val = today.getFullYear();
-val = today.getHours();
-val = today.getMinutes();
-val = today.getSeconds();
-val = today.getMilliseconds();
-val = today.getTime();
+// val = today.getMonth();
+// val = today.getDate();
+// val = today.getDay();
+// val = today.getFullYear();
+// val = today.getHours();
+// val = today.getMinutes();
+// val = today.getSeconds();
+// val = today.getMilliseconds();
+// val = today.getTime();
 
-birthday.setMonth(2);
-birthday.setDate(12);
-birthday.setFullYear(1985);
-birthday.setHours(3);
-birthday.setMinutes(30);
-birthday.setSeconds(25);
+// birthday.setMonth(2);
+// birthday.setDate(12);
+// birthday.setFullYear(1985);
+// birthday.setHours(3);
+// birthday.setMinutes(30);
+// birthday.setSeconds(25);
 
-console.log(birthday);
+// console.log(birthday);
+
+// IF STATEMENTS AND COMPARISON OPERATORS
+
+// if(something){
+//   do something
+// } else {
+//   do something else
+// }
+
+const id = 100;
+
+// // EQUAL TO
+// if (id == 100) {
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // NOT EQUAL TO
+// if (id != 101) {
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // EQUAL TO VALUE AND TYPE
+// if (id == 100) {
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // EQUAL TO VALUE AND TYPE
+// if (id !== 100) {
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// Test if undefined
+// if(typeof id !== 'undefined') {
+//   console.log(`The ID is ${id}`);
+// } else {
+//   console.log('NO ID');
+// }
+
+// GREATER OR LESS THAN
+// if(id <= 100) {
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// IF ELSE
+const color = 'yellow';
+
+// if (color === 'red') {
+//   console.log('Color is red');
+// } else if (color === 'blue') {
+//   console.log('Color is blue');
+// } else {
+//   console.log('Color is not red or blue');
+// }
+
+// LOGICAL OPERATORS
+const name = 'Steve';
+const age = 20;
+
+// AnD &&
+if (age > 0 && age < 12) {
+  console.log(`${name} is a child`);
+} else if (age >= 13 && age <= 19){
+  console.log(`${name} is a teenager`);
+} else {
+  console.log(`${name} is an adult`);
+}
+
+// OR ||
+if (age < 16 || age > 65) {
+ console.log(`${name} can not run race`);
+} else {
+  console.log(`${name} is registered for the race`);
+}
+
+// TERNARY OPERATOR
+console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
+
+// WITHOUT BRACES
+if (id === 100)
+  console.log('CORRECT');
+else
+  console.log('INCORRECT');
+
+  
