d93948e794c35a431b5a9f76088b05b2bec6f621
Art Todorov <art.todorov@me.com>
2019-12-26 22:10:56 -0800

    switches

--- app.js
+++ app.js
@@ -496,7 +496,7 @@ const id = 100;
 // }
 
 // IF ELSE
-const color = 'yellow';
+// const color = 'yellow';
 
 // if (color === 'red') {
 //   console.log('Color is red');
@@ -507,32 +507,75 @@ const color = 'yellow';
 // }
 
 // LOGICAL OPERATORS
-const name = 'Steve';
-const age = 20;
+// const name = 'Steve';
+// const age = 20;
 
 // AnD &&
-if (age > 0 && age < 12) {
-  console.log(`${name} is a child`);
-} else if (age >= 13 && age <= 19){
-  console.log(`${name} is a teenager`);
-} else {
-  console.log(`${name} is an adult`);
+// if (age > 0 && age < 12) {
+//   console.log(`${name} is a child`);
+// } else if (age >= 13 && age <= 19){
+//   console.log(`${name} is a teenager`);
+// } else {
+//   console.log(`${name} is an adult`);
+// }
+
+// // OR ||
+// if (age < 16 || age > 65) {
+//  console.log(`${name} can not run race`);
+// } else {
+//   console.log(`${name} is registered for the race`);
+// }
+
+// // TERNARY OPERATOR
+// console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
+
+// // WITHOUT BRACES
+// if (id === 100)
+//   console.log('CORRECT');
+// else
+//   console.log('INCORRECT');
+
+// SWITCHES
+const color = 'yellow';
+
+switch(color) {
+  case 'red':
+    console.log('Color is red');
+    break;
+  case 'blue':
+    console.log('Color is blue');
+    break;
+  default:
+    console.log('Color is not red or blue');
+    break;
 }
 
-// OR ||
-if (age < 16 || age > 65) {
- console.log(`${name} can not run race`);
-} else {
-  console.log(`${name} is registered for the race`);
+let day;
+
+switch(new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+    break;
 }
 
-// TERNARY OPERATOR
-console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
+console.log(`Today is ${day}`);
+console.log(new Date().getDay());
 
-// WITHOUT BRACES
-if (id === 100)
-  console.log('CORRECT');
-else
-  console.log('INCORRECT');
-
-  
