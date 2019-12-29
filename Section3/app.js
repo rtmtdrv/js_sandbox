b2ac7403b196c1123315d3db1be09edb5b451d94
Art Todorov <art.todorov@me.com>
2019-12-28 18:48:45 -0800

    DOM selector for single elements

--- Section3/app.js
+++ Section3/app.js
@@ -1,40 +1,76 @@
-let val;
+// let val;
 
-val = document;
-val = document.all;
-val = document.all[5];
-val = document.all.length;
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
+// val = document;
+// val = document.all;
+// val = document.all[5];
+// val = document.all.length;
+// val = document.head;
+// val = document.body;
+// val = document.doctype;
+// val = document.domain;
+// val = document.URL;
+// val = document.characterSet;
+// val = document.contentType;
 
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].id;
-val = document.forms[0].method;
-val = document.forms[0].action;
+// val = document.forms;
+// val = document.forms[0];
+// val = document.forms[0].id;
+// val = document.forms[0].method;
+// val = document.forms[0].action;
 
-val = document.links;
-val = document.links[5];
-val = document.links[0].id;
-val = document.links[0].className;
-val = document.links[0].classList[0];
+// val = document.links;
+// val = document.links[5];
+// val = document.links[0].id;
+// val = document.links[0].className;
+// val = document.links[0].classList[0];
 
-val = document.images;
+// val = document.images;
 
-val = document.scripts;
-val = document.scripts[2].getAttribute('src');
+// val = document.scripts;
+// val = document.scripts[2].getAttribute('src');
 
-let scripts = document.scripts;
+// let scripts = document.scripts;
 
-let scriptsArr = Array.from(scripts);
+// let scriptsArr = Array.from(scripts);
 
-scriptsArr.forEach(function(script){
-  console.log(script.getAttribute('src'));
-});
+// scriptsArr.forEach(function(script){
+//   console.log(script.getAttribute('src'));
+// });
 
-console.log(val);
+// console.log(val);
+
+// document.getElementById()
+
+// console.log(document.getElementById('task-title'));
+
+// const taskTitle = document.getElementById('task-title')
+
+// // Get things from the element
+// console.log(document.getElementById('task-title').id);
+// console.log(document.getElementById('task-title').className);
+
+// // Change Styling
+// taskTitle.style.background = "#333";
+// document.getElementById('task-title').style.color = "#fff";
+// document.getElementById('task-title').style.padding = "5px";
+// // document.getElementById('task-title').style.display = "none";
+
+// // Change Content
+// document.getElementById('task-title').textContent = 'Task List';
+// document.getElementById('task-title').innerText = 'My Tasks';
+// taskTitle.innerHTML = '<span style="color:red">Task List</span>';
+
+// document.querySelector()
+
+console.log(document.querySelector('#task-title'));
+console.log(document.querySelector('.card-title'));
+console.log(document.querySelector('h5'));
+
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'blue';
+
+document.querySelector('li:last-child').style.color = 'red';
+document.querySelector('li:nth-child(3)').style.color = 'yellow';
+document.querySelector('li:nth-child(4)').textContent = 'Hello World';
+document.querySelector('li:nth-child(odd)').style.background = '#ccc';
+document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
