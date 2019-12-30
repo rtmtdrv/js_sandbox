3afe797873ba0fd56dca3d79d0b72d7d2ec2ee8a
Art Todorov <art.todorov@me.com>
2019-12-29 19:43:49 -0800

    traversing the DOM

--- Section3/app.js
+++ Section3/app.js
@@ -1,130 +1,58 @@
-// let val;
+let val;
 
-// val = document;
-// val = document.all;
-// val = document.all[5];
-// val = document.all.length;
-// val = document.head;
-// val = document.body;
-// val = document.doctype;
-// val = document.domain;
-// val = document.URL;
-// val = document.characterSet;
-// val = document.contentType;
+const list = document.querySelector('ul.collection');
+const listItem = document.querySelector('li.collection-item:first-child');
 
-// val = document.forms;
-// val = document.forms[0];
-// val = document.forms[0].id;
-// val = document.forms[0].method;
-// val = document.forms[0].action;
+val = listItem;
+val = list;
 
-// val = document.links;
-// val = document.links[5];
-// val = document.links[0].id;
-// val = document.links[0].className;
-// val = document.links[0].classList[0];
+// Get Child Nodes
+val = list.childNodes;
+val = list.childNodes[0];
+val = list.childNodes[0].nodeName;
+val = list.childNodes[1].nodeType;
 
-// val = document.images;
-
-// val = document.scripts;
-// val = document.scripts[2].getAttribute('src');
-
-// let scripts = document.scripts;
-
-// let scriptsArr = Array.from(scripts);
-
-// scriptsArr.forEach(function(script){
-//   console.log(script.getAttribute('src'));
-// });
-
-// console.log(val);
-
-// document.getElementById()
-
-// console.log(document.getElementById('task-title'));
-
-// const taskTitle = document.getElementById('task-title')
-
-// // Get things from the element
-// console.log(document.getElementById('task-title').id);
-// console.log(document.getElementById('task-title').className);
-
-// // Change Styling
-// taskTitle.style.background = "#333";
-// document.getElementById('task-title').style.color = "#fff";
-// document.getElementById('task-title').style.padding = "5px";
-// // document.getElementById('task-title').style.display = "none";
-
-// // Change Content
-// document.getElementById('task-title').textContent = 'Task List';
-// document.getElementById('task-title').innerText = 'My Tasks';
-// taskTitle.innerHTML = '<span style="color:red">Task List</span>';
-
-// document.querySelector()
-
-// console.log(document.querySelector('#task-title'));
-// console.log(document.querySelector('.card-title'));
-// console.log(document.querySelector('h5'));
-
-// document.querySelector('li').style.color = 'red';
-// document.querySelector('ul li').style.color = 'blue';
-
-// document.querySelector('li:last-child').style.color = 'red';
-// document.querySelector('li:nth-child(3)').style.color = 'yellow';
-// document.querySelector('li:nth-child(4)').textContent = 'Hello World';
-// document.querySelector('li:nth-child(odd)').style.background = '#ccc';
-// document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
-
-// DOM Selectors For Multiple Elements
-
-// document.getElementsByClassName
-
-// const items = document.getElementsByClassName('collection-item');
-// console.log(items);
-// console.log(items[0]);
-// items[0].style.color = 'red';
-// items[3].textContent = 'Hello';
-
-// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
-// console.log(listItems);
+// 1 - Element
+// 2 - Attribute (deprecated)
+// 3 - Text node
+// 8 - Comment
+// 9 - Document itself
+// 10 - Doctype 
 
 
-// document.getElementsByTagName 
+// Get children element nodes
+val = list.children;
+val = list.children[1];
+list.children[1].textContent = 'Hello';
+// Children of children
+list.children[3].children[0].id = 'test-link';
+val = list.children[3].children;
 
-// let lis = document.getElementsByTagName('li');
-// // console.log(lis);
-// // console.log(lis[0]);
-// // lis[0].style.color = 'red';
-// // lis[3].textContent = 'Hello';
+// First child
+val = list.firstChild;
+val = list.firstElementChild;
 
-// // Convert HTML Collection into array
-// lis = Array.from(lis);
+// Last child
+val = list.lastChild;
+val = list.lastElementChild;
 
-// lis.reverse();
+// Count child elements
+val = list.childElementCount;
 
-// lis.forEach(function(li, index){
-//   console.log(li.className);
-//   li.textContent = `${index}: Hello`;
-// });
+// Get parent node
+val = listItem.parentNode;
+val = listItem.parentElement;
+val = listItem.parentElement.parentElement;
 
-// console.log(lis);
+// Get next sibling
+val = listItem.nextSibling;
+val = listItem.nextElementSibling;
+val = listItem.nextElementSibling.nextElementSibling.previousElementSibling;
 
-const items = document.querySelectorAll('ul.collection li.collection-item');
+// Get previous sibling
+// val = listItem.previousSibling;
+// val = listItem.previousElementSibling;
+// val = listItem.previousElementSibling.previousElementSibling;
 
-items.forEach(function(item, index){
-  item.textContent = `${index}: Hello`;
-});
 
-const liOdd = document.querySelectorAll('li:nth-child(odd)')
-const liEven = document.querySelectorAll('li:nth-child(even)')
-
-liOdd.forEach(function(li, index){
-  li.style.background = '#ccc';
-});
-
-for(let i = 0; i < liEven.length; i++){
-  liEven[i].style.background = '#f4f4f4';
-}
-
-console.log(items);
-
+console.log(val);
