f159129e5fbee82fbc82640e11f0928ba3522412
Art Todorov <art.todorov@me.com>
2020-01-01 19:40:24 -0800

    replace & remove classes/attributes

--- Section3/app.js
+++ Section3/app.js
@@ -1,29 +1,50 @@
-// Create element
+// REPLACE ELEMENT
 
-const li = document.createElement('li');
-
-// Add class
-li.className = 'collection-item';
-
+// Create Element
+const newHeading = document.createElement('h2');
 // Add id
-li.id = 'new-item';
+newHeading.id = 'task-title';
+// New text node
+newHeading.appendChild(document.createTextNode('Task List'));
 
-// Add attribute
-li.setAttribute('title', 'New Item');
+// Get the old heading
+const oldHeading = document.getElementById('task-title');
+// Parent
+const cardAction = document.querySelector('.card-action');
+cardAction.replaceChild(newHeading, oldHeading);
 
-// Create text node and append
-li.appendChild(document.createTextNode('Hello World'));
+// REMOVE ELEMENT
+const lis = document.querySelectorAll('li');
+const list = document.querySelector('ul');
 
-// Create new link element
-const link = document.createElement('a');
-// Add classes
-link.className = 'delete-item secondary-content';
-// Add icon html
-link.innerHTML = '<i class="fa fa-remove"></i>';
-// Append link into li
-li.appendChild(link);
+// Remove list item
+lis[0].remove();
 
-// Append li as child to ul
-document.querySelector('ul.collection').appendChild(li);
+// Remove child element
+list.removeChild(lis[3]);
 
-console.log(li);
+// CLASSES & ATTRIBUTES
+const firstLi = document.querySelector('li:first-child');
+const link = firstLi.children[0];
+
+let val;
+
+// Classes
+val = link.className;
+val = link.classList;
+val = link.classList[0];
+link.classList.add('test');
+link.classList.remove('test');
+val = link;
+
+// Attributes
+val = link.getAttribute('href');
+val = link.setAttribute('href', 'http://google.com');
+link.setAttribute('title', "Google")
+val = link.hasAttribute('title');
+val = link.removeAttribute('title');
+val = link;
+
+console.log(val);
+
+
