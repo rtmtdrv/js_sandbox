c4319a21c7125dc2e1c481bcc2e41a0f56adb70a
Art Todorov <art.todorov@me.com>
2020-01-05 00:43:33 -0800

    protypal inheritance

--- Section5/app.js
+++ Section5/app.js
@@ -1,50 +1,44 @@
-// PROTOTYPES EXPLAINED
+// PROTOTYPAL INHERITANCE
 
-// Object.prototype
-// Person.prototype
-
-// Person Constructor
-function Person(firstName, lastName, dob) {
+// Person constructor
+function Person(firstName, lastName){
   this.firstName = firstName;
   this.lastName = lastName;
-  this.birthday = new Date(dob);
-  // this.calculateAge = function(){
-  //   const diff = Date.now() - this.birthday.getTime();
-  //   const ageDate = new Date(diff);
-  //   return Math.abs(ageDate.getUTCFullYear() - 1970);
-  // }
 }
 
-// Calculate Age
-Person.prototype.calculateAge = function(){
-  const diff = Date.now() - this.birthday.getTime();
-  const ageDate = new Date(diff);
-  return Math.abs(ageDate.getUTCFullYear() - 1970);
+// Greeting
+Person.prototype.greeting = function(){
+  return `Hello there ${this.firstName} ${this.lastName}`;
 }
 
-// Get full name
-Person.prototype.getFullName = function(){
-  return `${this.firstName} ${this.lastName}`;
+const person1 = new Person('John', 'Doe');
+
+// console.log(person1.greeting());
+
+// Customer constructor
+function Customer(firstName, lastName, phone, membership){
+  Person.call(this, firstName, lastName);
+
+  this.phone = phone;
+  this.membership = membership;
 }
 
-// Gets Married
-Person.prototype.getsMarried = function(newLastName){
- this.lastName = newLastName;
+// Inherit the Person prototype methods
+Customer.prototype = Object.create(Person.prototype);
+
+// Make customer.prototype return Customer()
+Customer.prototype.constructor = Customer;
+
+// Create customer
+const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard');
+
+console.log(customer1);
+
+// Customer greeting
+Customer.prototype.greeting = function(){
+  return `Hello there ${this.firstName} ${this.lastName} welcome to our company`;
 }
 
-const john = new Person('John', 'Doe', '8-12-90');
-const mary = new Person('Mary', 'Swanson', '5-26-88');
-
-console.log(john, mary);
-console.log(john.calculateAge());
-console.log(mary.getFullName());
-
-mary.getsMarried('Smith');
-
-console.log(mary.getFullName());
-
-console.log(mary.hasOwnProperty('firstName'));
-console.log(mary.hasOwnProperty('getFullName'));
-
+console.log(customer1.greeting());
 
 
