2b94cfcecdff72ecf557523b859f90bd0de4ad93
Art Todorov <art.todorov@me.com>
2020-01-06 23:13:52 -0800

    es6 classes

--- Section5/app.js
+++ Section5/app.js
@@ -1,31 +1,38 @@
-// USING OBJECT.CREATE
+// ES6 CLASSES
 
-const personPrototypes = {
-  greeting: function() {
-    return `Hello there ${this.firstName} ${this.lastName}`;
-  },
-  getsMarried: function(newLastName) {
+class Person {
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthday = new Date(dob);
+  }
+
+  greeting() {
+    return `Hello there ${this.firstName} ${this.lastName}`
+  }
+
+  calculateAge() {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+
+  getsMarried(newLastName) {
     this.lastName = newLastName;
+  }
+  
+  // Static Method
+  static addNumbers(x, y) {
+    return x + y;
   }
 }
 
-const mary = Object.create(personPrototypes);
-mary.firstName = 'Mary';
-mary.lastName = 'Williams';
-mary.age = 30;
+const mary = new Person('Mary', 'Williams', '11-13-1980');
 
 mary.getsMarried('Thompson');
 
 console.log(mary);
 console.log(mary.greeting());
+console.log(mary.calculateAge());
 
-const brad = Object.create(personPrototypes, {
-  firstName: {value: 'Brad'},
-  lastName: {value: 'Traversy'},
-  age: {value: 36}
-});
-
-console.log(brad);
-console.log(brad.greeting());
-
-
+console.log(Person.addNumbers(1,2));
