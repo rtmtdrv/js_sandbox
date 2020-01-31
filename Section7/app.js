0a82d4a19a1d0cc8527e04d3aefc2841fef0572e
Art Todorov <art.todorov@me.com>
2020-01-30 17:09:02 -0800

    fetch api sandbox

--- Section7/app.js
+++ Section7/app.js
@@ -1,36 +1,58 @@
-const posts = [
-  {title: 'Post One', body: 'This is post one'},
-  {title: 'Post Two', body: 'This is post two'}
-];
+document.getElementById('button1').addEventListener('click', getText);
 
-function createPost(post) {
-  return new Promise(function(resolve, reject){
-    setTimeout(function(){
-      posts.push(post);
+document.getElementById('button2').addEventListener('click', getJson);
 
-      const error = true;
+document.getElementById('button3').addEventListener('click', getExternal);
 
-      if(!error) {
-        resolve();
-      } else {
-        reject('Error: Something went wrong!');
-      }
-    }, 2000);
-  });
-}
-
-function getPosts() {
-  setTimeout(function(){
-    let output = '';
-    posts.forEach(function(post){
-      output += `<li>${post.title}</li>`;
+// Get Local Text File Data
+function getText() {
+  fetch('test.txt')
+    .then(function(res){
+      return res.text();
+    })
+    .then(function(data){
+      console.log(data);
+      document.getElementById('output').innerHTML = data;
+    })
+    .catch(function(err){
+      console.log(err);
     });
-    document.body.innerHTML = output;
-  }, 1000);
 }
 
-createPost({title: 'Post Three', body: 'This is post three'})
-.then(getPosts)
-.catch(function(err) {
-  console.log(err);
-});
+// Get Local JSON Data
+function getJson() {
+  fetch('post.json')
+    .then(function(res){
+      return res.json();
+    })
+    .then(function(data){
+      console.log(data);
+      let output = '';
+      data.forEach(function(post){
+        output += `<li>${post.title}</li>`;
+      });
+      document.getElementById('output').innerHTML = output;
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+}
+
+// Get from External API
+function getExternal() {
+  fetch('https://api.github.com/users')
+    .then(function(res){
+      return res.json();
+    })
+    .then(function(data){
+      console.log(data);
+      let output = '';
+      data.forEach(function(user){
+        output += `<li>${user.login}</li>`;
+      });
+      document.getElementById('output').innerHTML = output;
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+}
