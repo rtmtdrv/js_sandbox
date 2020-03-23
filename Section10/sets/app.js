864418c2e5802835f96b8f1ea93887eea2f8df46
Art Todorov <art.todorov@me.com>
2020-03-23 15:34:07 -0700

    sets

---
+++ Section10/sets/app.js
@@ -0,0 +1,44 @@
+// SETS - Store unique values of any type
+
+const set1 = new Set();
+
+// Add values to set
+set1.add(100);
+set1.add('A string');
+set1.add({name: 'John'});
+set1.add(true);
+
+// const set2 = new Set([1, true, 'string']);
+
+// console.log(set1);
+// console.log(set2);
+
+// Get Count
+// console.log(set1.size);
+
+// Check for values
+// console.log(set1.has(100));
+// console.log(set1.has(50 + 50));
+// console.log(set1.has({name: 'John'}));
+
+// console.log(set1.has('hello' === 'hello'));
+
+// Delete from set
+// set1.delete(100);
+// console.log(set1);
+
+// Iterating through sets
+
+// For..of
+// for(let item of set1) {
+//   console.log(item);
+// }
+
+// ForEach Loop
+// set1.forEach((value) => {
+//   console.log(value);
+// });
+
+// CONVERT SET TO ARRAY
+const setArr = Array.from(set1);
+console.log(setArr);
